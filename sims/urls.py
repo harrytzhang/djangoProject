53e542bedce5a78aa6705f34346bc2b29c75af71
harrytzhang <2238803284@qq.com>
2024-07-04 10:57:02 +0800

    first commit

---
+++ sims/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from. import views
+
+urlpatterns = [
+    path('', views.index),
+    path('add/', views.add),
+    path('edit/', views.edit),
+    path('delete/', views.delete),
+]
